050acaf569f42ef3c3497a6f851b60ab2f74d3f2
FuZioNGP <fuzionsamp@hotmail.com>
2022-04-04 19:49:05 -0300

    Criando a função de codificar ou decodificar.

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
 int main()
 {
-    printf("Hello world");
+    char escolha;
+    printf("Ola Bem vindo ao sistema de codificacao e decodificacao\nInsira a baixo a Opção desejada:\nC - codificar\nD - Decodificar\n");
+    scanf("%c", &escolha);
+    if(escolha == 'C' || escolha == 'c')
+    {
+        printf("Escolha Codificar!\n");
+    }
+    else if (escolha == 'D' || escolha == 'd')
+    {
+        printf("Escolha Decodificar!\n");
+    }
+    else { 
+        printf("Escolha invalida!\n");
+    }
     return 0;
 }
