4383892a9ef858f1e2e4874666749021ae623e37
FuZioNGP <fuzionsamp@hotmail.com>
2022-04-04 20:22:31 -0300

    Adicionado as Funções de Codificar e Decodificar

--- main.c
+++ main.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+void Codificar(char texto[1000])
+{
+    printf("%s\n", texto);
+}
+void Decodificar(char texto[1000])
+{
+    printf("%s\n", texto);
+}
 int main()
 {
     char escolha;
-    printf("Ola Bem vindo ao sistema de codificacao e decodificacao\nInsira a baixo a Opção desejada:\nC - codificar\nD - Decodificar\n");
+    printf("Ola Bem vindo ao sistema de codificacao e decodificacao\nInsira a baixo a opcao desejada:\nC - codificar\nD - Decodificar\n");
     scanf("%c", &escolha);
     if(escolha == 'C' || escolha == 'c')
     {
+        char cod[1000] = "";
+        scanf("%s", &cod);
         printf("Escolha Codificar!\n");
+        Codificar(cod);
     }
     else if (escolha == 'D' || escolha == 'd')
     {
+        char cod[1000] = "";
+        scanf("%s", &cod);
         printf("Escolha Decodificar!\n");
+        Codificar(cod);
     }
     else { 
         printf("Escolha invalida!\n");
