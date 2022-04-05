91ac988304ec09db8c23e62dfa893df512838338
JL-conrado <joaolucasc@ucl.br>
2022-04-04 22:45:55 -0300

    HISTOGRAMA

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
+#define DIM      1000
+#define HIST_DIM 256
+
+//calcular quantas vezes cada letra se repete
+void calcular( int *hist, const char*str) {
+    memset( hist, 0, HIST_DIM * sizeof(int));
+    while(*str)
+        hist[(unsigned char) *(str++)]++;
+}
+
+
+int main(void) {
+    unsigned char c;
+    int histograma[HIST_DIM];
+    char texto[DIM] = "";
+
+    printf("Informe um texto: ");
+    gets(texto);
+
+    calcular( histograma, texto );
+
+    for( c = 'a'; c <= 'z'; c++ )
+        printf("%c = %d\n", c, histograma[c]);
+
+    return 0;
+}
+
 char convert[10] = {'A','B','C','D','E','F','G','H','I','J'}; //array para transformar numeros em letras
 
 //char convert[10] = "ABCDEFGHIJ";
@@ -19,9 +46,13 @@ void Decodificar(char texto[1000]) //FUNÇÃO PARA DECODIFICAR
     printf("%s\n", texto);
 }
 
-
+void DiferenciarZ(Char texto[1000]) // Função para fazer diferenciação de Z's
+{
+  if (texto)
+}
 int main() //MAIN 
 {
+  
     char escolha;
     printf("Ola Bem vindo ao sistema de codificacao e decodificacao\nInsira a baixo a opcao desejada:\nC - codificar\nD - Decodificar\n");
     scanf("%c", &escolha);
