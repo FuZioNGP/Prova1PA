e80d6232c7eb1cb716b223a81487ea20d3e444de
FuZioNGP <fuzionsamp@hotmail.com>
2022-04-05 14:45:37 -0300

    Identificando numeros e Letra Z

--- main.c
+++ main.c
@@ -1,43 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-#define DIM      1000
-#define HIST_DIM 256
-
-//calcular quantas vezes cada letra se repete
-void calcular( int *hist, const char*str) {
-    memset( hist, 0, HIST_DIM * sizeof(int));
-    while(*str)
-        hist[(unsigned char) *(str++)]++;
-}
-
-
-int main(void) {
-    unsigned char c;
-    int histograma[HIST_DIM];
-    char texto[DIM] = "";
-
-    printf("Informe um texto: ");
-    gets(texto);
-
-    calcular( histograma, texto );
-
-    for( c = 'a'; c <= 'z'; c++ )
-        printf("%c = %d\n", c, histograma[c]);
-
-    return 0;
-}
 
 char convert[10] = {'A','B','C','D','E','F','G','H','I','J'}; //array para transformar numeros em letras
 
 //char convert[10] = "ABCDEFGHIJ";
 void Codificar(char texto[1000]) //FUNÇÃO PARA CODIFICAR
 {
-    if(strcmp(texto, "Z") != 0) 
+    char *teste = strtok(texto,"");
+    for(int i = 0; i < strlen(texto); i++)
     {
-        printf("contem Z");
+        if(teste[i] >= '0' && teste[i] <= '9')
+        {
+            printf("\n%c <<< e um numero\n", teste[i]);
+        }
+        if(teste[i] == 'Z' || teste[i] == 'z')
+        {
+            printf("\n%c <-- e um Z",teste[i]);
+        }
+        printf("\n%c\n", teste[i]);
     }
-    printf("%s\n", texto);
 }
 
 
@@ -46,32 +28,33 @@ void Decodificar(char texto[1000]) //FUNÇÃO PARA DECODIFICAR
     printf("%s\n", texto);
 }
 
-void DiferenciarZ(Char texto[1000]) // Função para fazer diferenciação de Z's
+int main(void) //MAIN 
 {
-  if (texto)
-}
-int main() //MAIN 
-{
-  
+    void codificar(char texto[]);
+    void decodificar(char texto[]);
+
     char escolha;
     printf("Ola Bem vindo ao sistema de codificacao e decodificacao\nInsira a baixo a opcao desejada:\nC - codificar\nD - Decodificar\n");
     scanf("%c", &escolha);
+    
     if(escolha == 'C' || escolha == 'c')
     {
-        char cod[1000] = "";
-        scanf("%s", &cod);
+        char cod[1000];
+        scanf("%s", cod);
         printf("Escolha Codificar!\n");
         Codificar(cod);
+
     }
     else if (escolha == 'D' || escolha == 'd')
     {
-        char cod[1000] = "";
-        scanf("%s", &cod);
+        char cod2[1000];
+        scanf("%s", cod2);
         printf("Escolha Decodificar!\n");
-        Codificar(cod);
+        Decodificar(cod2);
     }
     else { 
         printf("Escolha invalida!\n");
     }
     return 0;
 }
+
